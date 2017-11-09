e4162a37779f8767bb962cc954fb6064521f4c89
Esther Trager <starbird@umich.edu>
2017-11-09 12:26:06 -0500

    Add PUT and DELETE methods.

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const fs = require('fs');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
@@ -33,12 +39,47 @@ app.post('/recipes', function (req, res) {
 	fs.readFile('recipes.json', (err, data) => {
 	  if (err) throw err;
 	  const recipes = JSON.parse(data);
-	  const id = recipes.length;
+	  const id = Date.now();
 	  recipe.id = id;
 	  recipes.push(recipe);
 	  fs.writeFile('recipes.json', JSON.stringify(recipes, null, 2), (err) => {
 	  	if (err) throw err;
 	  	res.send(recipe);
+	  });
+	})
+});
+
+app.put('/recipes/:id', function (req, res) {
+	const recipe = req.body;
+	fs.readFile('recipes.json', (err, data) => {
+	  if (err) throw err;
+	  const recipes = JSON.parse(data);
+	  const id = parseInt(req.params.id);
+	  const index = recipes.findIndex((_recipe) => {
+	  	return _recipe.id === id;
+	  });
+	  recipes[index] = recipe;
+	  fs.writeFile('recipes.json', JSON.stringify(recipes, null, 2), (err) => {
+	  	if (err) throw err;
+	  	res.send(recipe);
+	  });
+	})
+});
+
+app.delete('/recipes/:id', function (req, res) {
+	fs.readFile('recipes.json', (err, data) => {
+	  if (err) throw err;
+	  const recipes = JSON.parse(data);
+	  const id = parseInt(req.params.id);
+	  const index = recipes.findIndex((_recipe) => {
+	  	return _recipe.id === id;
+	  });
+	  console.log(`Found recipe with id = ${id} at index ${index}`);
+	  recipes.splice(index, 1);
+	  console.log(recipes);
+	  fs.writeFile('recipes.json', JSON.stringify(recipes, null, 2), (err) => {
+	  	if (err) throw err;
+	  	res.send({});
 	  });
 	})
 });
