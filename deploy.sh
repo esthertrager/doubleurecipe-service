e55f6ff3748210d59529a8537fcda7c674721a3c
Trager, Bradley <Bradley.Trager@cengage.com>
2017-11-17 11:34:04 -0500

    Update deploy and remove tar.

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 rm doubleurecipe-service.tar.gz
-gtar -cvzf doubleurecipe-service.tar.gz server
+gtar -cvzf doubleurecipe-service.tar.gz *
 scp doubleurecipe-service.tar.gz 192.34.60.247:/home/btrager/
 
-ssh 192.34.60.247 "rm -rf /opt/doubleurecipe-service/server/*
+ssh 192.34.60.247 "rm -rf /opt/doubleurecipe-service/*
 tar xzf ~/doubleurecipe-service.tar.gz -C /opt/doubleurecipe-service/
 rm ~/doubleurecipe-service.tar.gz"
